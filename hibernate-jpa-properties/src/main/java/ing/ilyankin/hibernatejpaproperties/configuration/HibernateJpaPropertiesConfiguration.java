4490808ec13e8876d72b016a8dd1c774bb733325
ilyankin <lomernap881@gmail.com>
2023-12-08 12:23:28 +0300

    module (hibernate-jpa-properties): add more native configuration method

--- hibernate-jpa-properties/src/main/java/ing/ilyankin/hibernatejpaproperties/configuration/HibernateJpaPropertiesConfiguration.java
+++ hibernate-jpa-properties/src/main/java/ing/ilyankin/hibernatejpaproperties/configuration/HibernateJpaPropertiesConfiguration.java
@@ -1,8 +1,13 @@
 package ing.ilyankin.hibernatejpaproperties.configuration;
 
 import lombok.SneakyThrows;
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 import java.io.FileInputStream;
@@ -11,14 +16,23 @@ import java.util.Properties;
 @Configuration
 public class HibernateJpaPropertiesConfiguration {
 
+//    @SneakyThrows
+//    @Bean("entityManagerFactory") // this bean name is mandatory to apply @PersistenceContext annotation support
+//    public LocalSessionFactoryBean sessionFactory() {
+//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//        sessionFactory.setPackagesToScan("ing.ilyankin.hibernatejpaproperties.model");
+//        Properties hibernateProperties = new Properties();
+//        hibernateProperties.load(new ClassPathResource("hibernate.properties").getInputStream());
+//        sessionFactory.setHibernateProperties(hibernateProperties);
+//        return sessionFactory;
+//    }
+
     @SneakyThrows
     @Bean("entityManagerFactory") // this bean name is mandatory to apply @PersistenceContext annotation support
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setPackagesToScan("ing.ilyankin.hibernatejpaproperties.model");
-        Properties hibernateProperties = new Properties();
-        hibernateProperties.load(new FileInputStream("./src/main/resources/hibernate.properties"));
-        sessionFactory.setHibernateProperties(hibernateProperties);
+        sessionFactory.setHibernateProperties(Environment.getProperties());
         return sessionFactory;
     }
 }
